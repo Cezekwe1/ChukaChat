56b1b5260072e59928242b3b5d97608a204f13e5
Cezekwe1 <eaezekwem@gmail.com>
2019-09-28 14:22:54 -0400

    finish up messages and add conversations

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var config = require('./server/config/config')
 var app = require('./server/server')
+var server = require('http').Server(app)
 var config = require('./server/config/config')
 var logger = require('./server/util/logger')
 var mongoose = require('mongoose')
+var io = require('socket.io').listen(server)
+var connections = []
+var users = []
 
 mongoose.connect(config.db.url, {useNewUrlParser: true, useUnifiedTopology: true})
 if (config.seed){
     require('./server/util/seed')
 }
-app.listen(config.port)
+
+
+server.listen(config.port)
 logger.log('listening on http://localhost:' + config.port)
