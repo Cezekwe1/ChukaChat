6fb162e3ec1743dce9a6e162d250446f0935d755
Cezekwe1 <eaezekwem@gmail.com>
2019-10-19 03:30:33 -0400

    add colors

--- server/util/logger.js
+++ server/util/logger.js
@@ -1,24 +1,22 @@
-var config = require('../config/config')
-var _ = require('lodash')
-require('colors')
-var noop = function(){}
-var consoleLog = config.logging ? console.log.bind(console) : noop
+var config = require("../config/config");
+var _ = require("lodash");
+require("colors");
+var noop = function() {};
+var consoleLog = config.logging ? console.log.bind(console) : noop;
 
 var logger = {
-    log: function(){
-        
-        var args = _.toArray(arguments)
-            .map(function(arg){
-                if (typeof arg === 'object'){
-                    var string = JSON.stringify(arg,2)
-                    return string.yellow
-                }else{
-                    arg += ''
-                    return arg.magenta
-                }
-            });
-        
-        consoleLog.apply(console,args)
-    }
-}
-module.exports = logger
+  log: function() {
+    var args = _.toArray(arguments).map(function(arg) {
+      if (typeof arg === "object") {
+        var string = JSON.stringify(arg, 2);
+        return string.yellow;
+      } else {
+        arg += "";
+        return arg.magenta;
+      }
+    });
+
+    consoleLog.apply(console, args);
+  }
+};
+module.exports = logger;
